91c9cea41e5ccb5e60ce674276d534504d593dfc
Hongxu <xuhongxu96@hotmail.com>
2019-04-10 23:08:46 +0800

    Finished loading FileList (UI).

--- ExamMaster.Wpf/MainWindow.xaml.cs
+++ ExamMaster.Wpf/MainWindow.xaml.cs
@@ -1,5 +1,6 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.IO;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
@@ -11,7 +12,7 @@ using System.Windows.Input;
 using System.Windows.Media;
 using System.Windows.Media.Imaging;
 using System.Windows.Navigation;
-using System.Windows.Shapes;
+using ExamMaster.Wpf.ViewModels;
 
 namespace ExamMaster.Wpf
 {
@@ -20,14 +21,47 @@ namespace ExamMaster.Wpf
     /// </summary>
     public partial class MainWindow : Window
     {
+        public FileList FileList { get; } = new FileList();
+
         public MainWindow()
         {
             InitializeComponent();
+
+            DataContext = this;
         }
 
         private void ButtonExit_Click(object sender, RoutedEventArgs e)
         {
             Application.Current.Shutdown();
+        }
+
+        private void BrowseButton_Click(object sender, RoutedEventArgs e)
+        {
+            using (var dialog = new System.Windows.Forms.FolderBrowserDialog()
+            {
+                SelectedPath = FileList.DirectoryPath,
+            })
+            {
+                if (dialog.ShowDialog() == System.Windows.Forms.DialogResult.OK)
+                {
+                    FileList.DirectoryPath = dialog.SelectedPath;
+                    FileList.DocxFiles.Clear();
+                    try
+                    {
+                        foreach (var item in Directory.EnumerateFileSystemEntries(
+                            FileList.DirectoryPath,
+                            "*.docx", SearchOption.AllDirectories))
+                        {
+                            var relativePath = Path.GetRelativePath(FileList.DirectoryPath, item);
+                            FileList.DocxFiles.Add(relativePath);
+                        }
+                    }
+                    catch (UnauthorizedAccessException)
+                    {
+                        MessageBox.Show("无权限访问此文件夹！", "错误", MessageBoxButton.OK, MessageBoxImage.Error);
+                    }
+                }
+            }
         }
     }
 }
---
+++ ExamMaster.Wpf/ViewModels/FileList.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using System.ComponentModel;
+using System.IO;
+using System.Text;
+
+namespace ExamMaster.Wpf.ViewModels
+{
+    public class FileList : INotifyPropertyChanged
+    {
+        private string _directoryPath = "";
+        public string DirectoryPath
+        {
+            get => _directoryPath;
+            set
+            {
+                _directoryPath = value;
+                PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(nameof(DirectoryPath)));
+            }
+        }
+
+        public ObservableCollection<string> DocxFiles { get; } = new ObservableCollection<string>();
+
+        public event PropertyChangedEventHandler PropertyChanged;
+    }
+}
